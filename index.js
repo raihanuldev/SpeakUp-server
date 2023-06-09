90833db79f7aa290c47102707a91fa7764aa49dc
Rihanul Islam <rihanthedev@gmail.com>
2023-06-09 18:38:23 +0600

    users get

--- index.js
+++ index.js
@@ -80,7 +80,10 @@ async function run() {
       res.send(result);
     })
   /************************************/  
-  
+  app.get('/users',async(req,res)=>{
+    const result = await usersCollection.find().toArray();
+    res.send(result)
+  })
   // Received Data.
   app.post('/users',async(req,res)=>{
     const user = req.body;
