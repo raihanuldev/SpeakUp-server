c5303fe6e3ea600a034b01833d447b08b1b2711b
Rihanul Islam <rihanthedev@gmail.com>
2023-06-11 19:11:41 +0600

    iNCREASE ADDED

--- index.js
+++ index.js
@@ -45,16 +45,16 @@ async function run() {
           $group: {
             _id: '$teacherId',
             totalEnrollments: { $sum: '$enrolled' },
-            instructorName: {$push: {instructor: '$instructorName'}},
-            instructorImage:{$push: {image: '$instructorImage'}}
+            instructorName: { $push: { instructor: '$instructorName' } },
+            instructorImage: { $push: { image: '$instructorImage' } }
           },
         },
         { $sort: { totalEnrollments: -1 } },
         { $limit: 6 }]).toArray();
-      
-      res.send(topinstructors) 
+
+      res.send(topinstructors)
     })
-// all Instructors
+    // all Instructors
     app.get('/instructors', async (req, res) => {
       try {
         const instructors = await couresCollection.aggregate([
@@ -68,7 +68,7 @@ async function run() {
             $replaceRoot: { newRoot: "$instructor" }
           }
         ]).toArray();
-    
+
         res.send(instructors);
       } catch (error) {
         console.error(error);
@@ -77,16 +77,27 @@ async function run() {
     });
 
     // Payment Releted Apis
-    app.post('/payments', async (req,res)=>{
+    app.post('/payments', async (req, res) => {
       const payment = req.body;
       const result = await paymentCollection.insertOne(payment);
       console.log(payment);
-      if(result.insertedId){
+      if (result.insertedId) {
         const cartId = payment.couresId;
-        const deleteResult =await cartCollection.deleteOne({cartId:cartId})
+        const deleteResult = await cartCollection.deleteOne({ cartId: cartId })
 
         if (deleteResult.deletedCount === 1) {
           console.log('Item removed from cartCollection');
+          const couresId = payment.couresId;
+          const updateResult = await couresCollection.updateOne(
+            { _id: new ObjectId(couresId) },
+            { $inc: { enrolled: 1 } }
+          );
+
+          if (updateResult.modifiedCount === 1) {
+            console.log('Enrolled count increased in couresCollection');
+          } else {
+            console.log('Failed to increase enrolled count in couresCollection');
+          }
         } else {
           console.log('Item not found in cartCollection');
         }
@@ -98,79 +109,79 @@ async function run() {
 
 
 
-// All Class
-    app.get('/allclasses', async (req,res)=>{
-      const apporvedCoures = await couresCollection.find({status:"approved"});
+    // All Class
+    app.get('/allclasses', async (req, res) => {
+      const apporvedCoures = await couresCollection.find({ status: "approved" });
       const result = await apporvedCoures.toArray();
       res.send(result);
     })
 
-    app.post('/newclass', async(req,res)=>{
+    app.post('/newclass', async (req, res) => {
       const item = req.body;
       console.log(item)
       const result = await couresCollection.insertOne(item);
       res.send(result)
     })
     // WARNING! IAM CHANGED USERS API>>>>
-// users Apis
-  app.get('/user', async(req,res)=>{
-    const email = req.query.email;
-    const query = {email:email}
-    // console.log(query);
-    const result = await usersCollection.findOne(query);
-    res.send(result)
-  })
-
-  // Received Data.
-  app.post('/users', async(req,res)=>{
-    const user = req.body;
-    console.log(user)
-    const query = {email:user.email};
-    const exitingUser =await usersCollection.findOne(query);
-    if(exitingUser){
-      return res.send({messgae: 'User Alredy exiting on Database'})
-    }
-    const result= await usersCollection.insertOne(user);
-    res.send(result);
-  })
-
-  // Carts apis
-  app.get('/carts', async(req,res)=>{
-    const email = req.query.email;
-    // console.log(email);
-    if(!email){
-      return res.send([])
-    }
-    const query = {email:email};
-    const result = await cartCollection.find(query).toArray();
-    res.send(result)
-  })
-  app.post('/carts', async(req,res)=>{
-    const item = req.body;
-    const cartId = {cartId:item.cartId}
-    const extingCart = await cartCollection.findOne(cartId);
-    if(extingCart){
-      return res.send([])
-    }
-    const result = await cartCollection.insertOne(item);
-    res.send(result);
-  })
-  // 
-  
-
-  // Payment Intent
-  app.post('/create-payment-intent', async (req,res)=>{
-    const {price} = req.body;
-    const amount = price*100;
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
-      currency:'usd',
-      payment_method_types: ['card']
-    });
-    res.send({
-      clientSecret: paymentIntent.client_secret
+    // users Apis
+    app.get('/user', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      // console.log(query);
+      const result = await usersCollection.findOne(query);
+      res.send(result)
     })
-  })
+
+    // Received Data.
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const query = { email: user.email };
+      const exitingUser = await usersCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ messgae: 'User Alredy exiting on Database' })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // Carts apis
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      // console.log(email);
+      if (!email) {
+        return res.send([])
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    })
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
+      const cartId = { cartId: item.cartId }
+      const extingCart = await cartCollection.findOne(cartId);
+      if (extingCart) {
+        return res.send([])
+      }
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+    // 
+
+
+    // Payment Intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
 
 
     await client.db("admin").command({ ping: 1 });
